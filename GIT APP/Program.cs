f365b3c4093bb69f530e9b59ec2d73abe5feb58c
Artemiy <isip_a.s.avdeev@mpt.ru>
2021-04-03 14:55:48 +0300

    fghjk

---
+++ GIT APP/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GIT_APP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World2");
+        }
+    }
+}
